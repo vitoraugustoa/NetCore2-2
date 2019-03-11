9b13972ac9935273cc84b83a6b65db8fa65f9b2b
vitoraugustoa <vitoraugustoa@hotmail.com>
2019-03-11 14:57:55 -0300

    Adicionando Autorização utilizando CLaims Policy

---
+++ MvcClaimsDemo/MvcClaimsDemo/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MvcClaimsDemo.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MvcClaimsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomeController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Claims = User.Claims;
+                
+            return View();
+        }
+
+        [Authorize(Policy = "CEO")]
+        public IActionResult Diretor()
+        {
+
+            return View();
+        }
+
+        [Authorize(Policy = "ADMIN")]
+        public IActionResult Funcionario()
+        {
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcClaimsDemo/MvcClaimsDemo/Controllers/SegurancaController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace MvcClaimsDemo.Controllers
+{
+    [Authorize(Policy = "FUNCI")]
+    public class SegurancaController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        public SegurancaController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity.Name != null)
+            {
+                if (User.Identity.Name == "diretor@email.com")
+                {
+                    var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                    //atribuir o valor da claim
+                    await _userManager.AddClaimAsync(user, new Claim("CEO", "D001"));
+                }
+                else
+                {
+                    var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                    //atribuir o valor da claim
+                    await _userManager.AddClaimAsync(user, new Claim("ADMIN", "A001"));
+                }
+                ViewBag.Claims = "Declaração atribuída a : " + User.Identity.Name.ToString();
+            }
+            else
+            {
+                ViewBag.Claims = "Não existe usuário logado";
+            }
+            return View();
+        }
+    }
+}
