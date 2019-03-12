37e387f94f75aefa0d67f0d43e94db3598b798f4
vitoraugustoa <vitoraugustoa@hotmail.com>
2019-03-12 12:54:02 -0300

    Utilizando Cors para gerenciar as requisições em minha WEB API

---
+++ ApiCors/ApiCors/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ApiCors
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            // Adicionar o serviço e cria uma policita com opções CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy" ,
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app , IHostingEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            /*
+             * Aplicação global da politica definida
+             * Certifique-se de declarar a funcionalidade CORS antes do MVC
+             * Para que o middleware seja acionado antes que o pipeline MVC
+             * Obtenha controle e encerre o pedido
+            */
+            app.UseCors("CorsPolicy");
+
+            app.UseMvc();
+        }
+    }
+}
