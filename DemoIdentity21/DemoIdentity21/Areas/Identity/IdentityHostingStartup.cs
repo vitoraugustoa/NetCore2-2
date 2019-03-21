c9d5238f4b9726061419a2681d2d549a7681fa66
vitoraugustoa <vitoraugustoa@hotmail.com>
2019-03-21 15:25:00 -0300

    Customizando Identity 2.1

---
+++ DemoIdentity21/DemoIdentity21/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DemoIdentity21.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DemoIdentity21.Areas.Identity.IdentityHostingStartup))]
+namespace DemoIdentity21.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
