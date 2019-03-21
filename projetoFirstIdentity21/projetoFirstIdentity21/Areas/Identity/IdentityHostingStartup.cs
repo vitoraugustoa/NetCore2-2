58f9ee303b59b91cfa1b254e6aa8fef2b67b3e63
vitoraugustoa <vitoraugustoa@hotmail.com>
2019-03-21 15:49:36 -0300

    Adicionar Identity 2.1 em um projeto uqe não tem autenticação

---
+++ projetoFirstIdentity21/projetoFirstIdentity21/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using projetoFirstIdentity21.Models;
+
+[assembly: HostingStartup(typeof(projetoFirstIdentity21.Areas.Identity.IdentityHostingStartup))]
+namespace projetoFirstIdentity21.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseMySql(
+                        context.Configuration.GetConnectionString("ConexaoMySql")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<AppDbContext>();
+            });
+        }
+    }
+}
